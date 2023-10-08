b315160bcad1047eb46e5559f75de6b4366cbb20
anhddse161412 <anhddse161412@fpt.edu.vn>
2023-10-08 15:37:57 +0700

    [ADD]: vnpay redirect

--- config/vnpayConfig.js
+++ config/vnpayConfig.js
@@ -3,5 +3,5 @@ module.exports = {
    vnp_HashSecret: "XCVWRFAZADPJLTUPPCPMUXCFAJKHMOSV",
    vnp_Url: "https://sandbox.vnpayment.vn/paymentv2/vpcpay.html",
    vnp_Api: "https://sandbox.vnpayment.vn/merchant_webapi/api/transaction",
-   vnp_ReturnUrl: "http://localhost:3000/payment/vnpay_return",
+   vnp_ReturnUrl: "https://fpt-sep.onrender.com/payment/vnpay_return",
 };
