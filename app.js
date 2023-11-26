53253fff44ed147b7c978f377706efcbe47c73f4
anhddse161412 <anhddse161412@fpt.edu.vn>
2023-11-26 20:10:25 +0700

    [FIX]: cors

--- app.js
+++ app.js
@@ -27,17 +27,21 @@ const connection = require("./config/db");
 var app = express();
 
 app.use(cors());
-app.use(logger("dev"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "js")));
-app.use(function (req, res, next) {
-   res.header("Access-Control-Allow-Origin", "*");
-   res.header(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content-Type, Accept"
+app.use((req, res, next) => {
+   res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+   res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE"
    );
+   res.setHeader(
+      "Access-Control-Allow-Headers",
+      "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
+   );
+   res.setHeader("Access-Control-Allow-Credentials", true);
+   res.setHeader("Access-Control-Allow-Private-Network", true);
+   //  Firefox caps this at 24 hours (86400 seconds). Chromium (starting in v76) caps at 2 hours (7200 seconds). The default value is 5 seconds.
+   res.setHeader("Access-Control-Max-Age", 7200);
+
    next();
 });
 
@@ -50,6 +54,12 @@ const io = new Server(httpServer, {
       origin: "http://localhost:3000",
    },
 });
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "js")));
 
 let onlineUsers = [];
 
